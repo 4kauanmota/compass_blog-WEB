a2b13221eca6d2732d118dc68d5e9a39bbc86835
Kauan Soares Mota <4kauanmota@gmail.com>
2023-07-17 23:10:40 -0300

    fix(index.html): post description limit

--- src/js/component/PostComponent.ts
+++ src/js/component/PostComponent.ts
@@ -11,12 +11,15 @@ export class PostComponent extends BaseComponent<HTMLElement, HTMLElement> {
     this.RenderContent();
   }
 
+  private textSize = Math.round(this.Element.querySelector('div > p')!.clientHeight / 16);
+
   RenderContent(): void {
     this.Element.setAttribute('onclick', 'window.location.href=' + `"/post/?id=${this.post.id}"`, )
     this.Element.querySelector('img')!.setAttribute('src', this.post.imgUrl);
     this.Element.querySelector('img')!.setAttribute('alt', `An image that represents ${this.post.title}`);
     this.Element.querySelector('div > h2')!.textContent = this.post.title;
     this.Element.querySelector('div > p')!.textContent = this.post.body;
+    this.Element.querySelector('div > p')!.setAttribute('style', `height: ${this.textSize * 2 - 0.15}ch; -webkit-line-clamp: ${this.textSize};`)
     this.Element.querySelector('footer > a')!.setAttribute('href', `/post/?id=${this.post.id}`)
   }
 }
